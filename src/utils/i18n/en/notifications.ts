0981edd7a46372e37f0527a76eef9ed64c677aeb
enzo.turpin <enzo.turpin@niji.fr>
2026-04-08 14:39:54 +0200

    refactor(i18n): séparer les fichiers bilingues en sous-dossiers en/ et fr/
    Chaque fichier i18n combinant les deux langues est remplacé par deux
    fichiers distincts dans src/utils/i18n/en/ et src/utils/i18n/fr/.
    L'index est mis à jour pour importer depuis les nouveaux chemins.
    
    Élimine ~98 % de duplication de code détectée par SonarQube sur ces
    fichiers (chaque clé de traduction était présente deux fois dans le
    même fichier source).

---
+++ src/utils/i18n/en/notifications.ts
@@ -0,0 +1,25 @@
+const notifications = {
+  notifications: {
+    title: "Notifications",
+    markAllRead: "Mark all read",
+    allRead: "All read!",
+    allReadMessage: "You have no notifications at the moment.",
+    timeAgo: {
+      justNow: "just now",
+      minutes: "{{count}} min",
+      hours: "{{count}}h ago",
+      yesterday: "Yesterday",
+      days: "{{count}}d ago",
+    },
+    declineConfirmTitle: "Decline Invitation",
+    declineConfirmMessage: "Are you sure you want to decline this invitation?",
+    declineError: "Unable to respond to this invitation.",
+    accept: "Accept",
+    decline: "Decline",
+    inviteAccepted: "{{inviter}} accepted your invitation",
+    inviteDeclined: "{{inviter}} declined your invitation",
+    inviteReceived: "{{inviter}} invites you to join a trip",
+  },
+};
+
+export default notifications;
--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -1,50 +1,64 @@
-import addresses from "./addresses";
-import auth from "./auth";
-import bookings from "./bookings";
-import common from "./common";
-import errors from "./errors";
-import friends from "./friends";
-import ideas from "./ideas";
-import invitations from "./invitations";
-import legal from "./legal";
-import notifications from "./notifications";
-import profile from "./profile";
-import tripManagement from "./tripManagement";
-import trips from "./trips";
+import enAddresses from "./en/addresses";
+import enAuth from "./en/auth";
+import enBookings from "./en/bookings";
+import enCommon from "./en/common";
+import enErrors from "./en/errors";
+import enFriends from "./en/friends";
+import enIdeas from "./en/ideas";
+import enInvitations from "./en/invitations";
+import enLegal from "./en/legal";
+import enNotifications from "./en/notifications";
+import enProfile from "./en/profile";
+import enTripManagement from "./en/tripManagement";
+import enTrips from "./en/trips";
+
+import frAddresses from "./fr/addresses";
+import frAuth from "./fr/auth";
+import frBookings from "./fr/bookings";
+import frCommon from "./fr/common";
+import frErrors from "./fr/errors";
+import frFriends from "./fr/friends";
+import frIdeas from "./fr/ideas";
+import frInvitations from "./fr/invitations";
+import frLegal from "./fr/legal";
+import frNotifications from "./fr/notifications";
+import frProfile from "./fr/profile";
+import frTripManagement from "./fr/tripManagement";
+import frTrips from "./fr/trips";
 
 export const resources = {
   en: {
     translation: {
-      ...common.en,
-      ...trips.en,
-      ...tripManagement.en,
-      ...bookings.en,
-      ...addresses.en,
-      ...friends.en,
-      ...invitations.en,
-      ...profile.en,
-      ...auth.en,
-      ...legal.en,
-      ...notifications.en,
-      ...ideas.en,
-      ...errors.en,
+      ...enCommon,
+      ...enTrips,
+      ...enTripManagement,
+      ...enBookings,
+      ...enAddresses,
+      ...enFriends,
+      ...enInvitations,
+      ...enProfile,
+      ...enAuth,
+      ...enLegal,
+      ...enNotifications,
+      ...enIdeas,
+      ...enErrors,
     },
   },
   fr: {
     translation: {
-      ...common.fr,
-      ...trips.fr,
-      ...tripManagement.fr,
-      ...bookings.fr,
-      ...addresses.fr,
-      ...friends.fr,
-      ...invitations.fr,
-      ...profile.fr,
-      ...auth.fr,
-      ...legal.fr,
-      ...notifications.fr,
-      ...ideas.fr,
-      ...errors.fr,
+      ...frCommon,
+      ...frTrips,
+      ...frTripManagement,
+      ...frBookings,
+      ...frAddresses,
+      ...frFriends,
+      ...frInvitations,
+      ...frProfile,
+      ...frAuth,
+      ...frLegal,
+      ...frNotifications,
+      ...frIdeas,
+      ...frErrors,
     },
   },
 };
